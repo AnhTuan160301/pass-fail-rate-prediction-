8bb4937a5142de726eb1e4f81358bbb6beb855c5
AnhTuan160301 <74240275+AnhTuan160301@users.noreply.github.com>
2023-05-31 19:18:59 +0700

    First Commit

---
+++ src/main/java/com/example/passrateprediction/PassFailRatePredictionApplication.java
@@ -0,0 +1,13 @@
+package com.example.passrateprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassFailRatePredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassFailRatePredictionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/passrateprediction/PassFailRatePredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.passrateprediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassFailRatePredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
